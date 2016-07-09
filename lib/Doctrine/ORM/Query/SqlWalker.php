7cf1a6a129cef1212f3fc5f1269d8f765d4b5149
Guilherme Blanco <guilhermeblanco@gmail.com>
2016-07-09 03:44:57 +0000

    Fixes to bugs and made test more consistent

--- lib/Doctrine/ORM/Query/SqlWalker.php
+++ lib/Doctrine/ORM/Query/SqlWalker.php
@@ -1943,7 +1943,7 @@ class SqlWalker implements TreeWalker
             $joinSqlParts = [];
 
             foreach ($joinColumns as $joinColumn) {
-                $property     = $class->getProperty($targetClass->fieldNames[$joinColumn['referencedColumnName']]);
+                $property     = $targetClass->getProperty($targetClass->fieldNames[$joinColumn['referencedColumnName']]);
                 $sourceColumn = $this->quoteStrategy->getJoinColumnName($joinColumn, $class, $this->platform);
                 $targetColumn = $this->quoteStrategy->getColumnName($property, $this->platform);
 
@@ -1969,7 +1969,7 @@ class SqlWalker implements TreeWalker
                     $this->parserResult->addParameterMapping($dqlParamKey, $this->sqlParamIndex++);
                 }
 
-                $sqlParts[] = $targetTableAlias . '.' . $targetColumnName . ' IN (' . $entitySql . ')';
+                $sqlParts[] = $targetTableAlias . '.' . $targetColumnName . ' = ' . $entitySql;
             }
 
             $sql .= implode(' AND ', $sqlParts);
